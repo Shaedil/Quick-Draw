e2705831e85dccf2dedb75409ce793449d9e9bc2
Jacob Koziej <jacobkoziej@gmail.com>
2020-08-14 16:03:07 -0400

    Initialize components

--- client/NodeMCU/NodeMCU.ino
+++ client/NodeMCU/NodeMCU.ino
@@ -1,5 +1,17 @@
+#define PUSHBUTTON D0
+const byte RGB[] = { D5, D6, D8, };
+
 void setup()
 {
+	pinMode(PUSHBUTTON, INPUT);
+
+	for (byte i = 0; i < sizeof(RGB) / sizeof(byte); i++) {
+		pinMode(RGB[i], OUTPUT);
+	}
+
+	Serial.flush();
+	Serial.begin(9600);
+
 }
 
 void loop()
