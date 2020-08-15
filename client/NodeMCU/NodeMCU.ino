1a4bdb791d1f3cc48ce6d3fd7b2cf4ad6143889b
Jacob Koziej <jacobkoziej@gmail.com>
2020-08-15 17:07:32 -0400

    Refactor into connect2AP
    Cleanup code for connecting to WiFi by moving it into its own function.

--- client/NodeMCU/NodeMCU.ino
+++ client/NodeMCU/NodeMCU.ino
@@ -14,11 +14,50 @@ struct communication {
 } packet;
 
 ICACHE_RAM_ATTR void playerReaction();
+void connect2AP();
 int handshake();
 
 ICACHE_RAM_ATTR void playerReaction()
 {
 	reaction_time = millis() - reaction_time;
+}
+
+void connect2AP() {
+	unsigned long connection_start = millis();
+	WiFi.begin(SSID, PASSWORD);
+
+	Serial.println("\033[2J");  // clear previous serial output
+
+	Serial.print("Connecting to: ");
+	Serial.println(SSID);
+
+	unsigned long wait = millis() - connection_start;
+
+	// Live update time waiting for connection
+	Serial.print("Waiting(ms): ");
+	Serial.print(wait);
+	do {
+		byte digits = 0;
+
+		while (wait) {
+			wait /= 10;
+			digits++;
+		}
+
+		for (byte i = 0; i < digits; i++) {
+			Serial.print("\b");
+		}
+
+		wait = millis() - connection_start;
+		Serial.print(wait);
+		delay(20);
+
+	} while (WiFi.status() != WL_CONNECTED);
+
+	Serial.println();
+
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
 int handshake() {
@@ -72,42 +111,7 @@ void setup()
 
 	attachInterrupt(digitalPinToInterrupt(PUSHBUTTON), playerReaction, RISING);
 
-	// Connect to WiFi
-	unsigned long connection_start = millis();
-	WiFi.begin(SSID, PASSWORD);
-
-	Serial.println("\033[2J");  // clear previous serial output
-
-	Serial.print("Connecting to: ");
-	Serial.println(SSID);
-
-	unsigned long wait = millis() - connection_start;
-
-	// Live update time waiting for connection
-	Serial.print("Waiting(ms): ");
-	Serial.print(wait);
-	do {
-		byte digits = 0;
-
-		while (wait) {
-			wait /= 10;
-			digits++;
-		}
-
-		for (byte i = 0; i < digits; i++) {
-			Serial.print("\b");
-		}
-
-		wait = millis() - connection_start;
-		Serial.print(wait);
-		delay(20);
-
-	} while (WiFi.status() != WL_CONNECTED);
-
-	Serial.println();
-
-	Serial.print("IP address: ");
-	Serial.println(WiFi.localIP());
+	connect2AP();  // Connect to specified access point
 }
 
 void loop()
